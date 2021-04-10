196544d6ceec0eed90b3a3c5471a0b7fa17a0667
adarpally <adarpally@drfirst.com>
2021-04-10 15:46:51 -0400

    first commit1

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Inside main");
+		System.out.println("Inside main123");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
